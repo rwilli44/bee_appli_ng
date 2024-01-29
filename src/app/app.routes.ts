9123b6f6ac2d75b72f3fe405ed0bf4c8a574af65
Rachel W. <rachelwilliams@mailbox.org>
2024-01-29 15:59:01 +0100

    added hives route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
 import { KeepersComponent } from './keepers/keepers.component';
+import { HivesComponent } from '../hives/hives.component';
 
-export const routes: Routes = [{ path: "keepers", component: KeepersComponent }];
+export const routes: Routes = [{ path: "keepers", component: KeepersComponent }, { path: "hives", component: HivesComponent }];
