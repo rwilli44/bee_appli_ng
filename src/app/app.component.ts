74a40c35e6f1dc936368acc1d762412c6cbdd487
Rachel W. <rachelwilliams@mailbox.org>
2024-01-29 12:37:30 +0100

    added keeper route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,10 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bee-appli';
+  title = 'Bee Appli 🐝';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { KeepersComponent } from './keepers/keepers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: "keepers", component: KeepersComponent }];
